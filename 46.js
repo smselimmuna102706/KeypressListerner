80234b5b27a2a5e411ddf3bf0cf8d245acb9da55
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-27 23:01:43 +0600

    Add files via upload

---
+++ 46.js
@@ -0,0 +1,10 @@
+
+
+var count = 0;
+
+document.querySelector("textarea").addEventListener("keypress",function(event){
+     count++;
+    var text = event.key;
+    document.querySelector("p").innerHTML = "You have press the : " + count;
+
+});
